c7d1bff18ff8c2571e225aceae49034e3e725d5f
johnsardor <sardorergashov282009@gmail.com>
2024-05-04 15:54:49 +0500

    html

--- t_bot/keyboards.py
+++ t_bot/keyboards.py
@@ -1,7 +1,7 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, WebAppInfo
 
-web_app = WebAppInfo(url='https://www.instagram.com')
-
+web_app = WebAppInfo(url='https://www.instagram.com/')
+# 'https://johnsardor.github.io/modul_11.github.io/
 
 apple_kb = ReplyKeyboardMarkup(keyboard=[
     [KeyboardButton(text='Mini Up', web_app=web_app)]
