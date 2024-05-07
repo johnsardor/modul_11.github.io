ccceb3b3bf54e1fb62a297e34726552e168ee421
johnsardor <sardorergashov282009@gmail.com>
2024-05-07 16:03:00 +0500

    update

---
+++ app.js
@@ -0,0 +1,16 @@
+let tg = window.Telegram.WebApp;
+tg.expand();
+
+tg.MainButton.textColor = "#FFFFFF"
+tg.MainButton.color = "#2cab37"
+
+let btn1 = document.getElementById("btn1")
+let btn2 = document.getElementById("btn2")
+let btn3 = document.getElementById("btn3")
+let btn4 = document.getElementById("btn4")
+let btn5 = document.getElementById("btn5")
+
+btn1.addEventListener("Click",function () {
+    tg.MainButton.setText("bnt1 bosildi")
+})
+
