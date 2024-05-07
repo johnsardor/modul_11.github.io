42cc962eaec451e85500671d93fc57e3b49eff7a
johnsardor <sardorergashov282009@gmail.com>
2024-05-07 16:26:14 +0500

    update push

--- app.js
+++ app.js
@@ -10,23 +10,23 @@ let btn3 = document.getElementById("btn3")
 let btn4 = document.getElementById("btn4")
 let btn5 = document.getElementById("btn5")
 
-btn1.addEventListener("Click",function () {
+btn1.addEventListener("click",function () {
     tg.MainButton.setText("bnt1 bosildi")
     tg.MainButton.show();
 })
-btn2.addEventListener("Click",function () {
+btn2.addEventListener("click",function () {
     tg.MainButton.setText("bnt2 bosildi")
     tg.MainButton.show();
 })
-btn3.addEventListener("Click",function () {
+btn3.addEventListener("click",function () {
     tg.MainButton.setText("bnt3 bosildi")
     tg.MainButton.show();
 })
-btn4.addEventListener("Click",function () {
+btn4.addEventListener("click",function () {
     tg.MainButton.setText("bnt4 bosildi")
     tg.MainButton.show();
 })
-btn5.addEventListener("Click",function () {
+btn5.addEventListener("click",function () {
     tg.MainButton.setText("bnt5 bosildi")
     tg.MainButton.show();
 })
