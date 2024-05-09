e5cd4db93549c28e1e33b859ec02f05c83c23e86
johnsardor <sardorergashov282009@gmail.com>
2024-05-09 15:40:01 +0500

    update push

--- app.js
+++ app.js
@@ -9,30 +9,38 @@ let btn2 = document.getElementById("btn2")
 let btn3 = document.getElementById("btn3")
 let btn4 = document.getElementById("btn4")
 let btn5 = document.getElementById("btn5")
+let item = ""
+
 
 btn1.addEventListener("click",function () {
     tg.MainButton.setText("bnt1 bosildi")
+    item = "Button bosildi";
     tg.MainButton.show();
 });
 btn2.addEventListener("click",function () {
     tg.MainButton.setText("bnt2 bosildi")
+    item = "Button2 bosildi";
+
     tg.MainButton.show();
 });
 btn3.addEventListener("click",function () {
     tg.MainButton.setText("bnt3 bosildi")
+    item = "Button3 bosildi";
     tg.MainButton.show();
 });
 btn4.addEventListener("click",function () {
     tg.MainButton.setText("bnt4 bosildi")
+    item = "Button4 bosildi";
     tg.MainButton.show();
 });
 btn5.addEventListener("click",function () {
     tg.MainButton.setText("bnt5 bosildi")
+        item = "Button5 bosildi";
     tg.MainButton.show();
 });
 
 Telegram.WebApp.onEvent("mainButtonClicked",function (){
-    tg.sendData("TestMessage")
+    tg.sendData(item)
 
 
 });
--- t_bot/handlers.py
+++ t_bot/handlers.py
@@ -1,4 +1,4 @@
-from aiogram import Dispatcher, types
+from aiogram import Dispatcher, types,F
 from aiogram.filters import CommandStart
 dp = Dispatcher()
 from keyboards import apple_kb
@@ -6,3 +6,12 @@ from keyboards import apple_kb
 @dp.message(CommandStart())
 async def start(msg: types.Message):
     await msg.answer("Salom", reply_markup=apple_kb)
+
+@dp.message(F.func(lambda msg: msg.web_app_data.data))
+async def get_btn(msg: types.Message):
+    await msg.answer(msg.web_app_data.data)
+
+
+
+
+
