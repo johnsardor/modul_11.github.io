c25c6ab478382fb0bb63bee3a8502cca848813a6
johnsardor <sardorergashov282009@gmail.com>
2024-05-09 15:15:35 +0500

    update push

--- app.js
+++ app.js
@@ -13,21 +13,33 @@ let btn5 = document.getElementById("btn5")
 btn1.addEventListener("click",function () {
     tg.MainButton.setText("bnt1 bosildi")
     tg.MainButton.show();
-})
+});
 btn2.addEventListener("click",function () {
     tg.MainButton.setText("bnt2 bosildi")
     tg.MainButton.show();
-})
+});
 btn3.addEventListener("click",function () {
     tg.MainButton.setText("bnt3 bosildi")
     tg.MainButton.show();
-})
+});
 btn4.addEventListener("click",function () {
     tg.MainButton.setText("bnt4 bosildi")
     tg.MainButton.show();
-})
+});
 btn5.addEventListener("click",function () {
     tg.MainButton.setText("bnt5 bosildi")
     tg.MainButton.show();
-})
+});
+
+Telegram.WebApp.onEvent("mainButtonClicked",function (){
+    tg.sendData("TestMessage")
+
+
+});
+
+
+
+
+
+
 
