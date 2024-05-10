c3b960a853ddb2f5cfdced70ecf866d04f77e17d
johnsardor <sardorergashov282009@gmail.com>
2024-05-10 15:15:19 +0500

    update push

--- app.js
+++ app.js
@@ -12,26 +12,26 @@ let item = ""
 
 
 btn1.addEventListener("click",function () {
-    tg.MainButton.setText("bnt1 bosildi")
+    tg.MainButton.setText("Burger bosildi")
     item = "Nomi :Burger" +
         "Narxi : 3$";
     tg.MainButton.show();
 });
 btn2.addEventListener("click",function () {
-    tg.MainButton.setText("bnt2 bosildi")
+    tg.MainButton.setText("Pepsi bosildi")
     item = "Nomi: Pepsi" +
         "Narxi: 1$";
 
     tg.MainButton.show();
 });
 btn3.addEventListener("click",function () {
-    tg.MainButton.setText("bnt3 bosildi")
+    tg.MainButton.setText("Cola bosildi")
     item = "Nomi: Cola" +
         "Narxi: 1$";
     tg.MainButton.show();
 });
 btn4.addEventListener("click",function () {
-    tg.MainButton.setText("bnt4 bosildi")
+    tg.MainButton.setText("KFC bosildi")
     item = "Nomi: KFC" +
         "Narxi: 6$";
     tg.MainButton.show();
