96af2f7d7d7623adc21bf795d60907ac476ff5cc
johnsardor <sardorergashov282009@gmail.com>
2024-05-10 15:07:25 +0500

    update push

--- app.js
+++ app.js
@@ -8,36 +8,35 @@ let btn1 = document.getElementById("btn1")
 let btn2 = document.getElementById("btn2")
 let btn3 = document.getElementById("btn3")
 let btn4 = document.getElementById("btn4")
-let btn5 = document.getElementById("btn5")
 let item = ""
 
 
 btn1.addEventListener("click",function () {
     tg.MainButton.setText("bnt1 bosildi")
-    item = "Button bosildi";
+    item = "Nomi :Burger" +
+        "Narxi : 3$";
     tg.MainButton.show();
 });
 btn2.addEventListener("click",function () {
     tg.MainButton.setText("bnt2 bosildi")
-    item = "Button2 bosildi";
+    item = "Nomi: Pepsi" +
+        "Narxi: 1$";
 
     tg.MainButton.show();
 });
 btn3.addEventListener("click",function () {
     tg.MainButton.setText("bnt3 bosildi")
-    item = "Button3 bosildi";
+    item = "Nomi: Cola" +
+        "Narxi: 1$";
     tg.MainButton.show();
 });
 btn4.addEventListener("click",function () {
     tg.MainButton.setText("bnt4 bosildi")
-    item = "Button4 bosildi";
+    item = "Nomi: KFC" +
+        "Narxi: 6$";
     tg.MainButton.show();
 });
-btn5.addEventListener("click",function () {
-    tg.MainButton.setText("bnt5 bosildi")
-        item = "Button5 bosildi";
-    tg.MainButton.show();
-});
+
 
 Telegram.WebApp.onEvent("mainButtonClicked",function (){
     tg.sendData(item)
