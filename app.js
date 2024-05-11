29807d0ea573e4fdeabc3909f68fde933582a9a5
johnsardor <sardorergashov282009@gmail.com>
2024-05-11 16:05:03 +0500

    update push

--- app.js
+++ app.js
@@ -1,52 +1,56 @@
+
 let tg = window.Telegram.WebApp;
 tg.expand();
 
 tg.MainButton.textColor = "#FFFFFF"
-tg.MainButton.color = "#2cab37"
+tg.MainButton.color = "#FC3005"
 
-let btn1 = document.getElementById("btn1")
-let btn2 = document.getElementById("btn2")
-let btn3 = document.getElementById("btn3")
-let btn4 = document.getElementById("btn4")
-let item = ""
+let rbtn1 = document.getElementById("rbtn1");
+let abtn1 = document.getElementById("abtn1");
+
+let rbtn2 = document.getElementById("rbtn2");
+let abtn2 = document.getElementById("abtn2");
+
+let rbtn3 = document.getElementById("rbtn3");
+let abtn3 = document.getElementById("abtn3");
+
+let rbtn4 = document.getElementById("rbtn4");
+let abtn4 = document.getElementById("abtn4");
+
+let item = " "
+let num_count = 0
+
+number = document.getElementById("count");
+
+abtn1.addEventListener("click", function () {
+
+    number.innerText = num_count += 1;
+    number.style.display = "block";
+})
 
 
-btn1.addEventListener("click",function () {
-    tg.MainButton.setText("Burger bosildi")
-    item = "Nomi :Burger" +
-        "Narxi : 3$";
+btn1.addEventListener("click", function () {
+    tg.MainButton.setText("Burger " + num);
+    item = "Burger/3$";
     tg.MainButton.show();
 });
-btn2.addEventListener("click",function () {
-    tg.MainButton.setText("Pepsi bosildi")
-    item = "Nomi: Pepsi" +
-        "Narxi: 1$";
+btn2.addEventListener("click", function () {
+    tg.MainButton.setText("KFC");
+    item = "KFC/10$";
+    tg.MainButton.show();
+});
+btn3.addEventListener("click", function () {
+    tg.MainButton.setText("LAVASH");
+    item = "LAVASH/4$";
+    tg.MainButton.show();
+});
+btn4.addEventListener("click", function () {
+    tg.MainButton.setText("Pizza");
+    item = "Pizza/5$";
 
     tg.MainButton.show();
 });
-btn3.addEventListener("click",function () {
-    tg.MainButton.setText("Cola bosildi")
-    item = "Nomi: Cola" +
-        "Narxi: 1$";
-    tg.MainButton.show();
+
+Telegram.WebApp.onEvent("mainButtonClicked", function () {
+    tg.sendData(item);
 });
-btn4.addEventListener("click",function () {
-    tg.MainButton.setText("KFC bosildi")
-    item = "Nomi: KFC" +
-        "Narxi: 6$";
-    tg.MainButton.show();
-});
-
-
-Telegram.WebApp.onEvent("mainButtonClicked",function (){
-    tg.sendData(item)
-
-
-});
-
-
-
-
-
-
-
