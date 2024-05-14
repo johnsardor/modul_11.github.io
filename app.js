960c6fb2ba6c3c720162d90c817fe001d294b7dc
johnsardor <sardorergashov282009@gmail.com>
2024-05-14 15:50:13 +0500

    update push

--- app.js
+++ app.js
@@ -5,103 +5,112 @@ tg.expand();
 tg.MainButton.textColor = "#FFFFFF"
 tg.MainButton.color = "#FC3005"
 
+// -----------------------1--------------------------
+
+let item1 = " "
+let n_count1 = 0
+
+
+count1 = document.getElementById("count1");
 let rbtn1 = document.getElementById("rbtn1");
 let abtn1 = document.getElementById("abtn1");
 
-let rbtn2 = document.getElementById("rbtn2");
-let abtn2 = document.getElementById("abtn2");
-
-let rbtn3 = document.getElementById("rbtn3");
-let abtn3 = document.getElementById("abtn3");
-
-let rbtn4 = document.getElementById("rbtn4");
-let abtn4 = document.getElementById("abtn4");
-
-let item = " "
-let num_count = 0
-
-number = document.getElementById("count");
-
 abtn1.addEventListener("click", function () {
+    count1.innerText = n_count1 += 1;
+    count1.style.display = "inline-block";
+    tg.MainButton.setText("Burger");
+    tg.MainButton.show();
+    item = "Burger/3/" + count1.innerText;
 
-    number.innerText = num_count += 1;
-    number.style.display = "inner-block";
-})
-rbtn1.addEventListener("click",function (){
-    number.innerText = num_count -= 1;
-    number.style.display = "inline-block";
-})
+});
+rbtn1.addEventListener("click", function () {
+    if (n_count1 > 0) {
+        count1.innerText = n_count1 -= 1;
+    }
+    count1.style.display = "inline-block";
+    item = "Burger/3/" + count1;
+    tg.MainButton.setText("Burger");
+    tg.MainButton.show();
+});
+// ------------------2--------------------
 
 count2 = document.getElementById("count2");
+rbtn2 = document.getElementById("rbtn2");
+abtn2 = document.getElementById("abtn2");
+let item2 = " ";
+let n_count2 = 0;
+rbtn2.addEventListener("click", function () {
+    if (n_count2 > 0) {
+        count2.innerText = n_count2 -= 1;
+    }
 
-abtn2.addEventListener("click", function () {
-    count2.innerText = n_count1 += 1;
     count2.style.display = "inline-block";
     tg.MainButton.setText("KFC");
     tg.MainButton.show();
-    item = "KfC/10/"+count2.innerText;
+});
 
-})
-rbtn2.addEventListener("click", function () {
-    count2.innerText = n_count2 -= 1;
+
+
+abtn2.addEventListener("click", function (){
+    count2.innerText = n_count2 += 1;
     count2.style.display = "inline-block";
-    item = "KFC/10/"+count2;
-})
+    tg.MainButton.setText("KFC");
+    tg.MainButton.show();
+});
+
+// ------------------3--------------------
 
 count3 = document.getElementById("count3");
+rbtn3 = document.getElementById("rbtn3");
+abtn3 = document.getElementById("abtn3");
+let item3 = " ";
+let n_count3 = 0;
+rbtn3.addEventListener("click", function () {
+    if (n_count3 > 0) {
+        count3.innerText = n_count3 -= 1;
+    }
 
-abtn3.addEventListener("click", function () {
+    count3.style.display = "inline-block";
+    tg.MainButton.setText("Pepsi");
+    tg.MainButton.show();
+});
+
+
+
+abtn3.addEventListener("click", function (){
     count3.innerText = n_count3 += 1;
     count3.style.display = "inline-block";
     tg.MainButton.setText("Pepsi");
     tg.MainButton.show();
-    item = "Pepsi/4/"+count1.innerText;
+});
 
-})
-rbtn3.addEventListener("click", function () {
-    count3.innerText = n_count3 -= 1;
-    count3.style.display = "inline-block";
-    item = "Pepsi/4/"+count3;
-})
+// -------------------4-------------------
 
 count4 = document.getElementById("count4");
+rbtn4 = document.getElementById("rbtn4");
+abtn4 = document.getElementById("abtn4");
+let item4 = " ";
+let n_count4 = 0;
+rbtn4.addEventListener("click", function () {
+    if (n_count4 > 0) {
+        count4.innerText = n_count4 -= 1;
+    }
 
-abtn4.addEventListener("click", function () {
-    count1.innerText = n_count4 += 1;
-    count1.style.display = "inline-block";
+    count4.style.display = "inline-block";
     tg.MainButton.setText("Cola");
     tg.MainButton.show();
-    item = "Cola/5/"+count4.innerText;
+});
 
-})
-rbtn4.addEventListener("click", function () {
-    count4.innerText = n_count4 -= 1;
+
+
+abtn4.addEventListener("click", function (){
+    count4.innerText = n_count4 += 1;
     count4.style.display = "inline-block";
-    item = "Cola/5/"+count4;
-})
+    tg.MainButton.setText("Cola");
+    tg.MainButton.show();
+});
 
 
-// abtn1.addEventListener("click", function () {
-//     tg.MainButton.setText("Burger " + num);
-//     item = "Burger/3$";
-//     tg.MainButton.show();
-// });
-// rbtn2.addEventListener("click", function () {
-//     tg.MainButton.setText("KFC");
-//     item = "KFC/10$";
-//     tg.MainButton.show();
-// });
-// abtn3.addEventListener("click", function () {
-//     tg.MainButton.setText("LAVASH");
-//     item = "LAVASH/4$";
-//     tg.MainButton.show();
-// });
-// rbtn4.addEventListener("click", function () {
-//     tg.MainButton.setText("Pizza");
-//     item = "Pizza/5$";
-//
-//     tg.MainButton.show();
-// });
 
 Telegram.WebApp.onEvent("mainButtonClicked", function () {
     tg.sendData(item);
--- t_bot/handlers.py
+++ t_bot/handlers.py
@@ -4,14 +4,14 @@ from aiogram.enums import ContentType
 from aiogram.filters import CommandStart, MagicData, callback_data
 from aiogram.utils.magic_filter import MagicFilter
 
-from keyboards import app_kb
+from keyboards import apple_kb
 
 dp = Dispatcher()
 
 
 @dp.message(CommandStart())
 async def start(msg: types.Message):
-    await msg.answer("Salom", reply_markup=app_kb)
+    await msg.answer("Salom", reply_markup=apple_kb)
 
 
 @dp.message(F.func(lambda msg: msg.web_app_data.data))
