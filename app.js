b3474c8e1aa87153915fb39c07c4f112b108e96e
johnsardor <sardorergashov282009@gmail.com>
2024-05-25 16:13:32 +0500

    update push

--- app.js
+++ app.js
@@ -187,33 +187,33 @@ abtn7.addEventListener("click", function () {
     tg.MainButton.show();
 });
 
-// count8 = document.getElementById("count8");
-// rbtn8 = document.getElementById("rbtn8");
-// abtn8 = document.getElementById("abtn8");
-// let item8 = " ";
-// let n_count8 = 0;
-// rbtn8.addEventListener("click", function () {
-//     if (n_count8 > 0) {
-//         count8.innerText = n_count8 -= 1;
-//     }
-//     item8 = "Molxona/40.000" + count8.innerText;
-//
-//     tg.MainButton.setText("Оплата");
-//     tg.MainButton.show();
-// });
-//
-//
-// abtn8.addEventListener("click", function () {
-//     count8.innerText = n_count8 += 1;
-//     count8.style.display = "inline-block";
-//     item8 = "Molxona/40.000" + count8.innerText;
-//
-//     tg.MainButton.setText("Оплата");
-//     tg.MainButton.show();
-// });
+count8 = document.getElementById("count8");
+rbtn8 = document.getElementById("rbtn8");
+abtn8 = document.getElementById("abtn8");
+let item8 = " ";
+let n_count8 = 0;
+rbtn8.addEventListener("click", function () {
+    if (n_count8 > 0) {
+        count8.innerText = n_count8 -= 1;
+    }
+    item8 = "Molxona/40.000" + count8.innerText;
+
+    tg.MainButton.setText("Оплата");
+    tg.MainButton.show();
+});
+
+
+abtn8.addEventListener("click", function () {
+    count8.innerText = n_count8 += 1;
+    count8.style.display = "inline-block";
+    item8 = "Molxona/40.000" + count8.innerText;
+
+    tg.MainButton.setText("Оплата");
+    tg.MainButton.show();
+});
 
 Telegram.WebApp.onEvent("mainButtonClicked", function () {
-    data = item1 + "|" + item2 + "|" + item3 + "|" + item4
+    data = item1 + "|" + item2 + "|" + item3 + "|" + item4 + item5 + "|" + item6 + item7 + "|" + item8
     tg.sendData(data);
 });
 
